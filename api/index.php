5f1657e69937ae06dfadd2f979610887c4b9eaba
Prince <jedmak23@icloud.com>
2026-08-20 14:49:00 +0200

    fix storage readonly

--- api/index.php
+++ api/index.php
@@ -1,17 +1,10 @@
 <?php
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-
-try {
-    require __DIR__ . '/../vendor/autoload.php';
-    $app = require_once __DIR__ . '/../bootstrap/app.php';
-    $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-    $response = $kernel->handle(
-        $request = Illuminate\Http\Request::capture()
-    );
-    $response->send();
-    $kernel->terminate($request, $response);
-} catch (Throwable $e) {
-    echo "<h1>ERREUR LARAVEL:</h1>";
-    echo "<pre>" . $e->getMessage() . "\n\n" . $e->getTraceAsString() . "</pre>";
-}
+use Illuminate\Http\Request;
+define('LARAVEL_START', microtime(true));
+$app = require __DIR__ . '/../bootstrap/app.php';
+$app->useStoragePath('/tmp/storage');
+require __DIR__ . '/../vendor/autoload.php';
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle($request = Request::capture());
+$response->send();
+$kernel->terminate($request, $response);
