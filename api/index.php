639e7adef0bbfb3e31ec7dc3cbaf3340a19ca558
Prince <jedmak23@icloud.com>
2026-08-20 14:52:37 +0200

    fix autoload order

--- api/index.php
+++ api/index.php
@@ -1,10 +1,15 @@
 <?php
 use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
-$app = require __DIR__ . '/../bootstrap/app.php';
-$app->useStoragePath('/tmp/storage');
+
 require __DIR__ . '/../vendor/autoload.php';
+
+$app = require __DIR__ . '/../bootstrap/app.php';
+$app->useStoragePath('/tmp');
+
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle($request = Request::capture());
+$response = $kernel->handle(
+    $request = Request::capture()
+);
 $response->send();
 $kernel->terminate($request, $response);
